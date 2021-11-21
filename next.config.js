8f0e9e7339e0358cef321e3a71c0d071b33e04a2
chogyejin <whrpwls96@naver.com>
2021-11-21 23:56:33 +0900

    Feat: Add API to get image in photos.tsx

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['via.placeholder.com'], // 외부 도메인에 있는 url 사용
+  },
+};
--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,10 +1,48 @@
 import HeadInfo from '../components/HeadInfo';
+import Image from 'next/image';
 
-export default function photos() {
+interface IPhoto {
+  albumid: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+interface Props {
+  photos: IPhoto[];
+}
+
+export default function photos({ photos }: Props) {
   return (
     <>
       <HeadInfo title="My Blog Photos" />
-      <div>My photos</div>
+      <h1>My photos</h1>
+      <ul>
+        {photos.map((photo, id) => (
+          <li key={id}>
+            <Image
+              src={photo.thumbnailUrl}
+              width={100}
+              height={100}
+              alt={photo.title}
+            />
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/photos?_start=0&_end=10`,
+  );
+  const photos = await res.json();
+  console.log(photos);
+  return {
+    props: {
+      photos,
+    },
+  };
+};
